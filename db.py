15e4021252e8785fe5960b3f6c5a81d187b36e26
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2025-02-21 14:28:38 -0700

    feat: db

--- db.py
+++ db.py
@@ -1,25 +1,41 @@
-from pymongo import MongoClient
-import bson
+# from pymongo import MongoClient
+# import bson
 
-# MongoDB setup
-client = MongoClient("mongodb+srv://corneliusardensatwikahermawan:9pRkkb17xcE1xU99@face-db.nqg8u.mongodb.net/")
-db = client["face_recognition"]
-collection = db["encodings"]
+# # MongoDB setup
+# client = MongoClient("mongodb+srv://corneliusardensatwikahermawan:9pRkkb17xcE1xU99@face-db.nqg8u.mongodb.net/")
+# db = client["face_recognition"]
+# collection = db["encodings"]
 
-def load_encodings():
-    try:
-        encodings = list(collection.find({}))
-        encodeList = [bson.Binary(enc['encoding']) for enc in encodings]
-        imgIds = [enc['face_id'] for enc in encodings]
-        return encodeList, imgIds
-    except Exception as e:
-        print(f"Error loading encodings: {e}")
-        return [], []
+# def load_encodings():
+#     try:
+#         encodings = list(collection.find({}))
+#         encodeList = [bson.Binary(enc['encoding']) for enc in encodings]
+#         imgIds = [enc['face_id'] for enc in encodings]
+#         return encodeList, imgIds
+#     except Exception as e:
+#         print(f"Error loading encodings: {e}")
+#         return [], []
 
-def insert_encoding(face_id: str, encoding: bytes):
-    try:
-        collection.insert_one({"face_id": face_id, "encoding": bson.Binary(encoding)})
-        return True
-    except Exception as e:
-        print(f"Error inserting encoding: {e}")
-        return False
+# def insert_encoding(face_id: str, encoding: bytes):
+#     try:
+#         collection.insert_one({"face_id": face_id, "encoding": bson.Binary(encoding)})
+#         return True
+#     except Exception as e:
+#         print(f"Error inserting encoding: {e}")
+#         return False
+
+import os
+import weaviate
+from weaviate.classes.init import Auth
+
+# Best practice: store your credentials in environment variables
+weaviate_url: str = os.getenv("URL")
+weaviate_api_key: str = os.getenv("API_KEY")
+
+# Connect to Weaviate Cloud
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url=weaviate_url,
+    auth_credentials=Auth.api_key(weaviate_api_key),
+)
+
+# print(client.is_ready())
