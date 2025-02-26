0fc72de100fdac476814e3b60c97265349f7d649
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2025-02-26 09:13:13 +0700

    fix db

--- db.py
+++ db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # MongoDB connection code (currently commented out)
 # from pymongo import MongoClient
 # import bson
@@ -48,32 +47,6 @@ weaviate = weaviate.Client(
 # Check if connection to Weaviate database is successful
 if weaviate.is_ready():
     print("Connected to Weaviate DB")
-=======
-# from pymongo import MongoClient
-# import bson
-
-# # MongoDB setup
-# client = MongoClient("mongodb+srv://corneliusardensatwikahermawan:9pRkkb17xcE1xU99@face-db.nqg8u.mongodb.net/")
-# db = client["face_recognition"]
-# collection = db["encodings"]
-
-# def load_encodings():
-#     try:
-#         encodings = list(collection.find({}))
-#         encodeList = [bson.Binary(enc['encoding']) for enc in encodings]
-#         imgIds = [enc['face_id'] for enc in encodings]
-#         return encodeList, imgIds
-#     except Exception as e:
-#         print(f"Error loading encodings: {e}")
-#         return [], []
-
-# def insert_encoding(face_id: str, encoding: bytes):
-#     try:
-#         collection.insert_one({"face_id": face_id, "encoding": bson.Binary(encoding)})
-#         return True
-#     except Exception as e:
-#         print(f"Error inserting encoding: {e}")
-#         return False
 
 import os
 import weaviate
@@ -90,4 +63,3 @@ client = weaviate.connect_to_weaviate_cloud(
 )
 
 # print(client.is_ready())
->>>>>>> origin/main
